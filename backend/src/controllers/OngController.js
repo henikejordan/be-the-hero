f31778d7010602f30ee45a6f03bd277700677258
Henike Jordan <henike_jordan@hotmail.com>
2020-03-27 19:22:07 -0300

    Inserindo testes

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
 module.exports = {
@@ -7,10 +7,11 @@ module.exports = {
     
     return response.json(ongs);
   },
+  
   async create(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = generateUniqueId();
 
     await connection('ongs').insert({
       id,
